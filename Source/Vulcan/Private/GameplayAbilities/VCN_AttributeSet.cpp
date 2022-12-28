7a3d6f3b5fe0f4055c2910fcf1e1eb0f68e312ec
mcdrak <c_guayara@javeriana.edu.co>
2022-12-28 16:46:50 -0500

    Added base attribute definitions

--- Source/Vulcan/Private/GameplayAbilities/VCN_AttributeSet.cpp
+++ Source/Vulcan/Private/GameplayAbilities/VCN_AttributeSet.cpp
@@ -3,3 +3,30 @@
 
 #include "GameplayAbilities/VCN_AttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+//======================================================================================================================
+UVCN_AttributeSet::UVCN_AttributeSet()
+{
+}
+
+//======================================================================================================================
+void UVCN_AttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UVCN_AttributeSet, MaxHealth, OldMaxHealth);
+}
+
+//======================================================================================================================
+void UVCN_AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UVCN_AttributeSet, Health, OldHealth);
+}
+
+//======================================================================================================================
+void UVCN_AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UVCN_AttributeSet, MaxHealth, COND_None, REPNOTIFY_OnChanged);
+	DOREPLIFETIME_CONDITION_NOTIFY(UVCN_AttributeSet, Health, COND_None, REPNOTIFY_OnChanged);
+}
--- Source/Vulcan/Public/GameplayAbilities/VCN_AttributeSet.h
+++ Source/Vulcan/Public/GameplayAbilities/VCN_AttributeSet.h
@@ -3,8 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "VCN_AttributeSet.generated.h"
+
+// Uses macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * TODO
@@ -13,4 +21,22 @@ UCLASS()
 class VULCAN_API UVCN_AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UVCN_AttributeSet();
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = "OnRep_MaxHealth")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UVCN_AttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = "OnRep_Health")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UVCN_AttributeSet, Health)
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth);
 };
